63b88007cfd9d27fa30c5f0f268fc9ad359a0558
Damir Mustafin <frenchthebread@gmail.com>
2016-07-26 18:20:11 +0300

    Update 0.2.0

--- bin/www.js
+++ bin/www.js
@@ -1,9 +1,64 @@
-var app     = require('../app');
-var config  = require('../config');
-var log     = require('../lib/log')(module);
+#!/usr/bin/env node
 
-var port = process.env.PORT || config.get('port');
+const app = require('../app');
+const debug = require('debug')('expressboilerplate:server');
+const http = require('http');
 
-app.listen(port, function() {
-    log.info('Listening on port ' + port);
-});
+const config = require('../config');
+
+const port = normalizePort(process.env.PORT || config.port);
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
--- config/mongoose.js
+++
@@ -1,6 +0,0 @@
-var mongoose = require('mongoose');
-var config = require('index');
-
-mongoose.connect(config.get('mongoose:uri'), config.get('mongoose:options'));
-
-module.exports = mongoose;
