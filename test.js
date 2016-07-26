d12c8884988f23697430eef0213bf8e250612df8
Damir Mustafin <frenchthebread@gmail.com>
2016-07-26 19:18:54 +0300

    Cleanup

--- test.js
+++ test.js
@@ -2,16 +2,6 @@ import test from 'ava';
 
 import request from 'request';
 
-test('foo', t => {
-    t.pass();
-});
-
-test('bar', async t => {
-    const bar = Promise.resolve('bar');
-
-    t.is(await bar, 'bar');
-});
-
 test('App is up', async t => {
 
   const statusCode = await new Promise((resolve, reject) => {
