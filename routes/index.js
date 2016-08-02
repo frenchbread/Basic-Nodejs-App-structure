d88d064cfdebe53f95045ab01817f20ae3c729fd
Damir Mustafin <frenchthebread@gmail.com>
2016-08-02 14:24:42 +0300

    Add title to home route

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const router  = express.Router();
 //home
 router.get('/', function(req, res) {
     res.render('index', {
+      title: 'express-boilerplate',
       header: 'express-boilerplate',
       description: 'Your basic web-app structure.'
     });
