1c3f3d21cf98ddb6788a531326aca7778ad829d0
Damir Mustafin <frenchthebread@gmail.com>
2016-08-02 14:56:43 +0300

    Updated tests

--- test.js
+++ test.js
@@ -1,15 +1,54 @@
 import test from 'ava';
+import r from 'request';
+import cheerio from 'cheerio';
 
-import request from 'request';
+import config from './config';
 
-test('App is up', async t => {
+const url = config.host + ':' + config.port;
+const errUrl = url + '/random-page';
 
-  const statusCode = await new Promise((resolve, reject) => {
-    request('http://127.0.0.1:3000/', (err, res, body) => {
+async function getPageAsElement (url) {
+  return await new Promise((resolve, reject) => {
+    r(url, (err, res, body) => {
       if (err) reject(err);
-      resolve(res.statusCode);
+      resolve(cheerio.load(body));
     });
   });
+};
 
-  t.is(statusCode, 200);
+// OK pages (200)
+test('Page title is correct', async t => {
+
+  const $ = await getPageAsElement(url);
+
+  t.is($('title').text(), 'express-boilerplate');
+});
+
+test('h1 text is correct', async t => {
+
+  const $ = await getPageAsElement(url);
+
+  t.is($('h1').text(), 'express-boilerplate');
+});
+
+test('p text is correct', async t => {
+
+  const $ = await getPageAsElement(url);
+
+  t.is($('p').text(), 'Your basic web-app structure.');
+});
+
+// Error pages (404)
+test('404 page has corrct title', async t => {
+
+  const $ = await getPageAsElement(errUrl);
+
+  t.is($('title').text(), 'Error');
+});
+
+test('404 page has corrct h1', async t => {
+
+  const $ = await getPageAsElement(errUrl);
+
+  t.is($('h1').text(), 'Not Found');
 });
